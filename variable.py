cd6cf7b66f4c0d38fd9aa248fd683c56d0241a70
eugenio martinez <emartinez714@gmail.com>
2026-04-23 17:00:24 -0400

    Float variables decimales example

--- variable.py
+++ variable.py
@@ -21,6 +21,21 @@ age = 35 # variable assignment (stores the value 21 in the variable age)
 quantity = 3 # variable assignment (stores the value 3 in the variable quantity)
 num_of_students = 30 # variable assignment (stores the value 30 in the variable num_of_students)
 
-print(f"You are {age} years old") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
-print(f"You are buying {quantity} items") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
-print(f"You class has {num_of_students} students") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
+#print(f"You are {age} years old") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
+#print(f"You are buying {quantity} items") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
+#$print(f"You class has {num_of_students} students") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
+
+
+#Float variables
+pi = 3.14 # variable assignment (stores the value 3.14 in the variable pi)
+gravity = 9.8 # variable assignment (stores the value 9.8 in the variable gravity)
+temperature = 98.6 # variable assignment (stores the value 98.6 in the variable temperature)
+
+price = 10.99 # variable assignment (stores the value 10.99 in the variable price)
+gpa = 3.2 # variable assignment (stores the value 3.2 in the variable gpa)
+distance = 5.5 # variable assignment (stores the value 5.5 in the variable distance)
+
+print(f"The price is ${price}") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
+print(f"Your gpa is {gpa}") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
+print(f"You ran {distance} miles") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
+
