d05d6f415d8a32e11c2e34729276fadffd44ec9e
eugenio martinez <emartinez714@gmail.com>
2026-04-23 15:01:15 -0400

    This is a simple Python script that demonstrates the use of variables in Python

---
+++ variable.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# This is a simple Python script that demonstrates the use of variables in Python.
+# variable = A container for value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+first_name =  "Bro"
+last_name = "code"
+
+print(first_name)
