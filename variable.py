fe3b794111cbeb67a561ce01ab9f8ab8afb6278d
eugenio martinez <emartinez714@gmail.com>
2026-04-23 16:18:51 -0400

    Integers variables numerico) example

--- variable.py
+++ variable.py
@@ -12,6 +12,15 @@ email = "Bro123@fake.com" # variable assignment (stores the value "Bro123@fake.c
 
 #print(f"Hello {first_name}") # f-string (formatted string literal)
 #print("hello " + first_name) # concatenation
-print("Hello", first_name) # comma (adds a space between the string and the variable
-print(f"You like {food}") # f-string (formatted string literal)
-print(f"Your email is: {email}") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
+#print("Hello", first_name) # comma (adds a space between the string and the variable
+#print(f"You like {food}") # f-string (formatted string literal)
+#print(f"Your email is: {email}") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
+
+#Integers variables
+age = 35 # variable assignment (stores the value 21 in the variable age)
+quantity = 3 # variable assignment (stores the value 3 in the variable quantity)
+num_of_students = 30 # variable assignment (stores the value 30 in the variable num_of_students)
+
+print(f"You are {age} years old") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
+print(f"You are buying {quantity} items") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
+print(f"You class has {num_of_students} students") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
