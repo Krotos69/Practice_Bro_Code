817af6a5e18fa4b6418c8c70276385ba3da8d686
eugenio martinez <emartinez714@gmail.com>
2026-04-24 09:08:37 -0400

    calculates the area of a rectangle based on user variable, input and typecasting

---
+++ Rectangle_Area_Calc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# This program calculates the area of a rectangle based on user input for length and width.
+# Get the length of the rectangle from the user
+
+length = input("Enter the length: ") # enter the lenght as floats before calculating the area
+width = input("Enter the width: ") # enter the  width as floats before calculating the area
+#length = float(length) # Convert the length to a float
+#width = float(width) # Convert the width to a float
+
+area = length * width # Calculate the area of the rectangle
+print(area) # Print the area of the rectangle to the console
