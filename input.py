ffaa0b138c3726fc262b154c971b72224e0c2501
eugenio martinez <emartinez714@gmail.com>
2026-04-24 08:40:10 -0400

    converts the age from a string to an integer, typecasting using int(age)

--- input.py
+++ input.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
-# input () function taht prompts the user to enter data
+# input () function that prompts the user to enter data
 # Returns the entered data as a string
 
 #input() # prompts the user to enter data and returns the entered data as a string
 name = input("What is your name?") # prompts the user to enter their name and returns the entered name as a string
 age  = input("How old are you?:") # prompts the user to enter their age and returns the entered age as a string
 
-print(f"Hello {name}!") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
-print(f"You are {age} years old") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
+age = int(age) # converts the age from a string to an integer
+age =age + 1 # adds 1 to the age (simulating a birthday)
 
+print(f"Hello {name}!") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
+print("Happy Birthday!") # prints "Happy Birthday!" to the console
+print(f"You are {age} years old") # f-string (formatted string literal) (allows you to embed expressions inside string literals, using curly braces {})
